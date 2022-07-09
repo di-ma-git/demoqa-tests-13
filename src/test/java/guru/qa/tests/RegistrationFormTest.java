3365301616d91bca09da805e8f66d9245f97df05
Dima Maltsev <phoenixnety@gmail.com>
2022-07-09 16:38:36 +0300

    removed usused imports

--- src/test/java/guru/qa/tests/RegistrationFormTest.java
+++ src/test/java/guru/qa/tests/RegistrationFormTest.java
@@ -1,10 +1,7 @@
 package guru.qa.tests;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
-import java.io.File;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
